aa1b24ff41e586a23bd07eae0c9d2432186f76f0
vaishnavi185 <vishu032004@gmail.com>
2024-06-06 22:20:10 +0530

    its half

---
+++ RotateLeft.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class RotateLeft {
+    int size;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("enter array size:")
+    
+    int[] arr = new int[size];
+    public static void main(String args[]){
+
+    }
+}
