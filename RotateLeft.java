e510260099ca9c916aa3cc23f68500c5cc206c46
vaishnavi185 <vishu032004@gmail.com>
2024-06-07 13:06:49 +0530

    rotateleft an array by one

--- RotateLeft.java
+++ RotateLeft.java
@@ -1,12 +1,22 @@
 import java.util.Scanner;
+import java.util.Arrays;;
 
 public class RotateLeft {
-    int size;
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("enter array size:")
-    
-    int[] arr = new int[size];
+   int n = 5;
+   int[] arr = {2,3,4,5,5};
     public static void main(String args[]){
+        RotateLeft l = new RotateLeft();
+        l.arr = l.Left(l.arr);
+        System.out.println(Arrays.toString(l.arr));
 
+
+
+    }
+    public int[] Left(int[] arr) {
+        int temp =arr[0];
+        for(int i=1; i<n;i++){
+            arr[i-1]=arr[i];
+        } arr[n-1]=temp;
+        return arr;
     }
 }
