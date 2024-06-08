2d9c13d2334ed828993fe512367216dc6e869535
vaishnavi185 <vishu032004@gmail.com>
2024-06-08 19:42:15 +0530

    complete

---
+++ RotateByArray.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RotateByArray {
+    int d ;
+    int[] arr = {1,2,3,4,5,6};
+    int n=6;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter how much you want to rotate:");
+    d=sc.nextInt();
+    public static void main(String[] args){
+
+    }
+
+    public int[] rotate(int[] arr){
+        
+    }
+
+    
+}
