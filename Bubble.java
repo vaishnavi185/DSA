984bb8b119cb51aae1dd9c938b90425149f21492
vaishnavi <vishu032004@gmail.com>
2025-02-19 11:45:15 +0530

    bubble sort

---
+++ Bubble.java
@@ -0,0 +1,22 @@
+class Bubble{
+
+     public static void sort(int[] nums){
+        int n= nums.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i-1;j++){
+                if ((nums[j]>nums[j+1])) {
+                    int temp=nums[j];
+                    nums[j]= nums[j+1];
+                    nums[j+1]= temp;
+                }
+            }
+        }
+     }
+    public static void main(String[] args){
+           int[] nums={1,99,0,8,7}; 
+           sort(nums);
+           for(int num:nums){
+            System.out.print(num+"");
+           } 
+          }
+}
