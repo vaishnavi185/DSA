5f13f50638b7c2d02e1e16f45468fb573fc9371d
vaishnavi <vishu032004@gmail.com>
2025-02-27 19:46:05 +0530

    stack using linklist

--- Stack.java
+++ Stack.java
@@ -1,22 +1,60 @@
+class Node{
+    int data;
+    Node next;
 
-public class Stack {
-    private java.util.Stack<Integer> stack = new java.util.Stack<>();
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
 
-    public void push(int value) {
-        stack.push(value);
+class Stack{
+    Node head;
+    int size;
+    public void Push(int val){
+       Node newnode = new Node(val);
+       if(head==null){
+        head = newnode;
+       }else{
+     newnode.next =head;
+        head= newnode;
+       }
+
+       size++;
+
+    }
+
+    public int Pop(){
+        if(head==null){
+            return 0;
+        }
+        else{
+            Node temp = head;
+            head=head.next;
+            temp=null;
+        size--;
+        return head.data;
+    }
+    }
+
+    public int peek(){
+        if(head==null){
+            return 0;
+        }
+        else{
+            return head.data;
+        }
     }
 
     public static void main(String[] args) {
-        Stack stack = new Stack();
-        stack.push(1);
-        
-        
-
-      
-        
-          
-
-
-
+        Stack s = new Stack();
+        s.Push(0);
+        s.Push(9);
+        s.Push(10);
+        s.Pop();
+        s.Pop();
+        System.out.println(s.peek());
     }
+
+
 }
