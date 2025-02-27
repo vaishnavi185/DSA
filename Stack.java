2376147238c62dae28ad2de4113fcbb7a32e2f6d
vaishnavi <vishu032004@gmail.com>
2025-02-27 19:24:09 +0530

    stack

---
+++ Stack.java
@@ -0,0 +1,22 @@
+
+public class Stack {
+    private java.util.Stack<Integer> stack = new java.util.Stack<>();
+
+    public void push(int value) {
+        stack.push(value);
+    }
+
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        stack.push(1);
+        
+        
+
+      
+        
+          
+
+
+
+    }
+}
