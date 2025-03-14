9de8eea41db393236f9b7a8b397ae5e7b2b4dc5c
vaishnavi <vishu032004@gmail.com>
2025-03-14 20:11:42 +0530

    empty

--- Summ.java
+++ Summ.java
@@ -1,6 +1,6 @@
 class Summ{
     public static void main(String[] args){
-         int[] nums={-1,0,1,9,2};
+         int[] nums={-1,0,1,0,2};
     
          
          for(int i=0;i<nums.length-2;i++){
