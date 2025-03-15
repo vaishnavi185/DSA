89c2d88247fba81f4fea1650666ea3747ddd464d
vaishnavi <vishu032004@gmail.com>
2025-03-15 12:00:05 +0530

    substring

---
+++ Longest.java
@@ -0,0 +1,13 @@
+public class Longest {
+    public static void main(String[] args) {
+        String str ="pwwkew";
+
+        int n = str.length();
+        int count =0;
+        int max=0;
+        for(int i=0;i<n;i++){
+           
+            
+        }
+    
+}}
