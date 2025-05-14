651979bf215d513253a1cac4a57fa594482b7260
vaishnavi <vishu032004@gmail.com>
2025-05-14 18:05:03 +0530

    transpose

---
+++ MatrixRotation.java
@@ -0,0 +1,38 @@
+public class MatrixRotation {
+    public static void transpose(int[][] mat){
+        int n= mat.length;
+        for(int i=0;i<n-1;i++){
+            for(int j = i+1;j<n;j++){
+                int temp=mat[i][j];
+                mat[i][j]=mat[j][i];
+                mat[j][i]=temp;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+       int[][] matrix = {
+    {1, 2, 3},
+    {4, 5, 6},
+    {7, 8, 9}
+};
+         
+         for(int i=0;i<matrix.length;i++){
+              for(int j=0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+" ");
+              }
+              System.out.println();}
+              transpose(matrix);
+        for (int i=0;i<matrix.length;i++){
+              for(int j=0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+" ");
+              }
+              System.out.println();
+         }
+         
+
+
+
+
+    }
+}
