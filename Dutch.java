1c9f580e66961736e368602d29c5dade0eb5dc56
vaishnavi <vishu032004@gmail.com>
2025-05-14 12:24:53 +0530

    nothinhg

--- Dutch.java
+++ Dutch.java
@@ -24,7 +24,7 @@ public class Dutch{
         }return arr;
     }
     public static void main(String[] args) {
-        int[] arr ={0,0,1,0,0,2,1};
+        int[] arr ={0,0,1,0,0,2,0};
         Dutch d=new Dutch();
         d.flag(arr);
         for(int i=0;i<arr.length;i++){
